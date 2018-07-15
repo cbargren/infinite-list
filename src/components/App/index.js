ac3149edf5c0893f0e9aa7b8007df445718a7e46
Christopher Bargren <cbargren@gmail.com>
2018-07-14 17:03:12 -0700

    Adding redux packages and rearranging

---
+++ src/components/App/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App = () => (
+  <div>
+    <h1>Pokédex</h1>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-const App = () => {
-  return (
-    <div>
-      <h1>Pokédex</h1>
-    </div>
-  );
-};
-render(<App />, document.getElementById('app'));
+import { Provider } from 'react-redux';
+
+import store from './store';
+import App from './components/App';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
